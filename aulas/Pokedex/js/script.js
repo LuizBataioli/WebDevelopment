b31ce2705c080311ae5cce92030889069d0db97b
Luiz Bataioli <136658924+LuizBataioli@users.noreply.github.com>
2024-06-26 22:39:13 -0300

    Pokedex finalizada

--- aulas/Pokedex/js/script.js
+++ aulas/Pokedex/js/script.js
@@ -1,6 +1,10 @@
 const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImage = document.querySelector('.pokemon__image');
+const pokemonWeight = document.querySelector('.pokemon_peso');
+const pokemonType = document.querySelector('.pokemon_tipo');
+const pokemonSound = document.querySelector('.pokemon_sound');
+const backgroundAudio = document.querySelector('.background_audio')
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
@@ -8,6 +12,7 @@ const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 
 let searchPokemon = 1;
+backgroundAudio.volume = 0.1;
 
 const fetchPokemon = async(pokemon) => {
     //Await faz ao código esperar a resposta da API para seguir para as proximas linhas
@@ -19,6 +24,7 @@ const fetchPokemon = async(pokemon) => {
     if(APIResponse.status == 200){
       //metodo json para extrair os dados em json da API
     const data = await APIResponse.json();
+    console.log(data)
     return data; 
 
     }
@@ -30,7 +36,10 @@ const fetchAlternativeData = async() => {
     return {
         name: 'Tata',
         id: '1026',
-        image: pokemonImage.src ='./icone/tata.png'
+        image: pokemonImage.src ='./icone/tata.png',
+        weight: pokemonWeight.innerHTML = '100 kg',
+        type: pokemonType.innerHTML = 'Miau',
+        sound: pokemonSound.src = './audio/miau2.mp3'
     };
 }
 
@@ -57,6 +66,13 @@ const renderPokemon = async(pokemon) => {
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
     input.value = '';
     searchPokemon = data.id;
+
+    pokemonSound.src = data['cries']['latest'];
+    pokemonSound.volume = 0.5
+
+    pokemonWeight.innerHTML = data.weight / 10 + ' kg';  // Peso em kg
+    pokemonType.innerHTML = data.types.map(type => type.type.name).join(', ');
+
     }else{
         pokemonImage.style.display = 'none';
         pokemonName.innerHTML = 'Not found';
